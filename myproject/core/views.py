d4fe2f3d90184fa38b2205b871a883bbdf5df8e0
alex <acayllahu@unsa.edu.pe>
2025-01-02 12:14:19 -0500

    primer commit

---
+++ myproject/core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+# core/views.py
+
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'core/home.html')
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
